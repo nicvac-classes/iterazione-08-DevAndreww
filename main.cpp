1c3ddaff659374daed07f14c01e22876c8adf9b3
DevAndreww <andrea.sette2@marconibari.edu.it>
2024-10-10 07:59:20 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,35 @@
 using namespace std;
 
 int main() {
+ int i, n, debito, promosso;
     string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    double voto, somma, media;
+
+    cout << "Quanti studenti ci sono?" << endl;
+    cin >> n;
+    i = 0;
+    debito = 0;
+    promosso = 0;
+    somma = 0;
+    while (i < n) {
+        i = i + 1;
+        cout << "Come ti chiami?" << endl;
+        cin >> nome;
+        cout << "Quanto hai preso?" << endl;
+        cin >> voto;
+        somma = somma + voto;
+        if (voto >= 6) {
+            cout << "Sei stato promosso." << endl;
+            promosso = promosso + 1;
+        } else {
+            cout << "Hai preso il debito..." << endl;
+            debito = debito + 1;
+        }
+    }
+    media = somma / n;
+    cout << "Saranno promossi " << promosso << " persone, e prenderanno il debito " << debito << " persone." << endl;
+    cout << "La media della classe è " << media << "." << endl;
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
